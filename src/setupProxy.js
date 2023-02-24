f068865b3f7855ca8a89fb5c242c05aa5c237e9f
Saverio Caminiti <s.caminiti@k-tech.it>
2023-02-24 13:13:25 +0100

    https and proxy in local env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,12 @@
 // Proxy to Django restapi and login urls in dev env only
 
 // @ts-ignore
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // const djangoUrl = 'https://krm3.k-tech-it/';  // Local
 const djangoUrl = 'http://localhost:8000/';  // Local
 
 module.exports = function (app) {
-	app.use(proxy('/v1/api', {target: djangoUrl, changeOrigin: true}));
+	app.use('/v1/api', createProxyMiddleware({target: djangoUrl, changeOrigin: true}));
 };
+
