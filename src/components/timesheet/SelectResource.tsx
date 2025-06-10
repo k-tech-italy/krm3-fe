82aa24a925f97b89f8bfe7dd7d36e73f04259f30
davide <davidecampana@icloud.com>
2025-06-10 09:42:11 +0200

    style select

--- src/components/timesheet/SelectResource.tsx
+++ src/components/timesheet/SelectResource.tsx
@@ -1,55 +1,67 @@
 import { useEffect } from "react";
-import Select, { SingleValue } from 'react-select';
+import Select, { SingleValue } from "react-select";
 import { useGetCurrentUser } from "../../hooks/useAuth";
 import { useGetActiveResources } from "../../hooks/useMissions";
 
+const SelectResourceComponent = ({
+  setSelectedResourceId,
+}: {
+  setSelectedResourceId: (id: number | null) => void;
+}) => {
+  const resources = useGetActiveResources();
+  const { data } = useGetCurrentUser();
 
-const SelectResourceComponent = ({ setSelectedResourceId }: { setSelectedResourceId: (id: number | null) => void }) => {
+  const getSelectOptions = () => {
+    if (!resources) return [];
+    return resources.map((resource) => ({
+      value: resource.id,
+      label: `${resource.firstName} ${resource.lastName}`,
+    }));
+  };
 
-    const resources = useGetActiveResources();
-    const { data } = useGetCurrentUser();
+  useEffect(() => {
+    // If no resource is selected, set the current user's resource as default
+    // TODO Handle the case in which the current user does not have a resource
+    if (data?.resource?.id) {
+      setSelectedResourceId(data.resource.id);
+    }
+  }, [data]);
 
-    const getSelectOptions = () => {
-        if (!resources) return [];
-        return resources.map(resource => ({
-            value: resource.id,
-            label: `${resource.firstName} ${resource.lastName}`
-        }));
-    };
+  const handleResourceChange = (
+    selectedOption: SingleValue<{ value: number; label: string }>
+  ) => {
+    setSelectedResourceId(selectedOption ? selectedOption.value : null);
+  };
 
-    useEffect(() => {
-        // If no resource is selected, set the current user's resource as default
-        // TODO Handle the case in which the current user does not have a resource
-        if (data?.resource?.id) {
-            setSelectedResourceId(data.resource.id);
-        }
-    }, [data]);
-
-
-    const handleResourceChange = (selectedOption: SingleValue<{ value: number; label: string }>) => {
-        setSelectedResourceId(selectedOption ? selectedOption.value : null);
-    };
-
-    return (
-        <>
-            {resources && resources.length > 0 ? (
-                <>
-                <div className="mb-4">
-                    <label htmlFor="resource-select">Select Resource:</label><br />
-                    <Select
-                        onChange={handleResourceChange}
-                        options={getSelectOptions()}
-                        isClearable
-                        placeholder="Select a resource"
-                        id="resource-select"
-                        isSearchable
-                        defaultValue={data?.resource ? { value: data.resource.id, label: `${data.resource.firstName} ${data.resource.lastName}` } : null}
-                    />
-                </div>
-                </>
-            ) : null}
-        </>
-    );
-}
+  return (
+    <>
+      {resources && resources.length > 0 ? (
+        <div className="flex justify-start">
+          <div className="mb-4">
+            <label htmlFor="resource-select" >Select Resource:</label>
+            <br />
+            <Select
+              onChange={handleResourceChange}
+              options={getSelectOptions()}
+              className="mt-2"
+              isClearable
+              placeholder="Select a resource"
+              id="resource-select"
+              isSearchable
+              defaultValue={
+                data?.resource
+                  ? {
+                      value: data.resource.id,
+                      label: `${data.resource.firstName} ${data.resource.lastName}`,
+                    }
+                  : null
+              }
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default SelectResourceComponent;
