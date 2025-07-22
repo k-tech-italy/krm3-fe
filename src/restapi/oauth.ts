3943086276aa4ae1c9fd8ed1dd0308b8c7cf972e
know46 <krzysiek.nooworyta@gmail.com>
2025-07-22 14:16:03 +0200

    wip

--- src/restapi/oauth.ts
+++ src/restapi/oauth.ts
@@ -3,11 +3,27 @@ import { restapi } from "./restapi";
 const oauthProvider = "google-oauth2";
 
 const LS_TOKEN_KEY = "Token";
+const AUTH_COOKIE_KEY = "auth-token";
 const LS_LOGIN_NEXT_URI = "next";
+
+function setAuthCookie(token: string | null) {
+    if (token) {
+        // Set cookie to expire in 7 days, adjust as needed
+        const d = new Date();
+        d.setTime(d.getTime() + (7*24*60*60*1000));
+        const expires = "expires="+ d.toUTCString();
+        document.cookie = `${AUTH_COOKIE_KEY}=${token};${expires};path=/`;
+    } else {
+        // Delete cookie
+        document.cookie = `${AUTH_COOKIE_KEY}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    }
+}
 
 export function loginUser(username: string, password: string) {
   return restapi.post("jwt/create/", { username, password }).then((res) => {
-    localStorage.setItem(LS_TOKEN_KEY, res.data.access);
+    const token = res.data.access;
+    localStorage.setItem(LS_TOKEN_KEY, token);
+    setAuthCookie(token);
   });
 }
 
@@ -15,6 +31,7 @@ export async function loginGoogle() {
   console.log("Logging in with Google");
   try {
     localStorage.removeItem(LS_TOKEN_KEY);
+    setAuthCookie(null);
     let currentUrl = window.location.toString();
     const loginUrl =
       window.location.protocol + "//" + window.location.host + "/login";
@@ -57,12 +74,14 @@ export async function googleAuthenticate(state: string, code: string) {
       });
       const token = res.data.access;
       localStorage.setItem(LS_TOKEN_KEY, token);
+      setAuthCookie(token);
 
       // redirect to LS_LOGIN_NEXT_URI or to /
       const next = localStorage.getItem(LS_LOGIN_NEXT_URI) || "/";
       window.location.replace(next);
     } catch (err) {
       localStorage.removeItem(LS_TOKEN_KEY);
+      setAuthCookie(null);
     }
   }
 }
@@ -71,12 +90,14 @@ export function getToken() {
   return localStorage.getItem(LS_TOKEN_KEY);
 }
 export function clearToken() {
-  return localStorage.removeItem(LS_TOKEN_KEY);
+  localStorage.removeItem(LS_TOKEN_KEY);
+  setAuthCookie(null);
 }
 
 export function refreshToken() {
   // we currently do not refresh automatically
   // the token is deleted so that a new login will be required
   localStorage.removeItem(LS_TOKEN_KEY);
+  setAuthCookie(null);
   // TODO when we get the token there is also a res.data.refresh, we store and use it
 }
